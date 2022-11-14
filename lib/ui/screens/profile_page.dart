9fc3d4b6411289d022e6039a8954067b45ee8bd5
Ravindu <rtjayasooriya@gmail.com>
2022-11-14 12:58:06 +0530

    profile page complete

--- lib/ui/screens/profile_page.dart
+++ lib/ui/screens/profile_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:plant_app/constants.dart';
+import 'package:plant_app/ui/screens/widgets/profile_widget.dart';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({Key? key}) : super(key: key);
@@ -10,10 +12,78 @@ class ProfilePage extends StatefulWidget {
 class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
-      body: Center(
-        child: Text("Profile Page"),
+        body: SingleChildScrollView(
+      child: Container(
+        padding: const EdgeInsets.all(10),
+        height: size.height,
+        width: size.width,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              width: 150,
+              child: CircleAvatar(
+                radius: 60,
+                backgroundColor: Colors.transparent,
+                backgroundImage:
+                    ExactAssetImage('assets/images/profilePic.jpg'),
+              ),
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                border: Border.all(
+                  color: Constants.primaryColor.withOpacity(0.5),
+                  width: 5,
+                ),
+              ),
+            ),
+            const SizedBox(height: 10),
+            SizedBox(
+              width: size.width * 0.4,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Profile name",
+                    style: TextStyle(
+                      color: Constants.blackColor,
+                      fontSize: 20,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 24,
+                    child: Image.asset('assets/images/verified.png'),
+                  ),
+                ],
+              ),
+            ),
+            Text(
+              "useremail@gmail.com",
+              style: TextStyle(
+                color: Constants.blackColor.withOpacity(0.3),
+              ),
+            ),
+            const SizedBox(height: 30),
+            SizedBox(
+              height: size.height * 0.6,
+              width: size.width,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: const [
+                  ProfileWidget(icon: Icons.person, title: "My Profile",),
+                  ProfileWidget(icon: Icons.settings, title: "Settings",),
+                  ProfileWidget(icon: Icons.notifications, title: "Notifications",),
+                  ProfileWidget(icon: Icons.chat, title: "FAQs",),
+                  ProfileWidget(icon: Icons.share, title: "Share",),
+                  ProfileWidget(icon: Icons.logout, title: "Log Out",),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
-    );
+    ));
   }
 }
+
